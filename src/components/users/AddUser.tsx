7241fae5434b99299c5830079da2354d81d7e040
godspeed7 <pawelpikus7@gmail.com>
2022-02-14 19:35:50 +0100

    add user validation form

--- src/components/users/AddUser.tsx
+++ src/components/users/AddUser.tsx
@@ -1,20 +1,58 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import Layout from "./Layout";
 
 const AddUser = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [validated, setValidated] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+  });
+  const [errors, setErrors] = useState({ name: "", email: "" });
+  console.log(errors);
+  // `PropertyKey` is short for "string | number | symbol"
+  // since an object key can be any of those types, our key can too
+  // in TS 3.0+, putting just "string" raises an error
+  function hasKey<O>(obj: O, key: PropertyKey): key is keyof O {
+    return key in obj;
+  }
 
-  const handleSubmit = (event: any) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+  const setField = (field: string, value: string) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
+    if (hasKey(errors, field)) {
+      if (!!errors[field])
+        setErrors({
+          ...errors,
+          [field]: null,
+        });
     }
+  };
 
-    setValidated(true);
+  type Errors = {
+    name: string;
+    email: string;
+  };
+  const findFormErrors = () => {
+    const { name, email } = form;
+    const newErrors: Errors = { name: "", email: "" };
+    if (!name || name === "") newErrors.name = "Fill in this field.";
+    else if (name.length > 30) newErrors.name = "Name is too long.";
+    if (!email) newErrors.email = "Fill in this field.";
+    return newErrors;
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newErrors = findFormErrors();
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      alert("Thank you for your feedback!");
+    }
   };
 
   return (
@@ -22,33 +60,43 @@ const AddUser = () => {
       <Row className="border bg-light mb-4 py-4">
         <h4>Add new User</h4>
       </Row>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form noValidate onSubmit={handleSubmit}>
         <Row className="mb-3 ">
-          <Form.Group as={Row} classname="mb-3" controlId="validationCustom01">
+          <Form.Group as={Row} controlId="validationCustom01">
             <Form.Label column sm="2">
               Name
             </Form.Label>
             <Col sm="10">
-              <Form.Control required type="text" placeholder="Your Name" />
+              <Form.Control
+                onChange={(e) => setField("name", e.target.value)}
+                isInvalid={!!errors.name}
+                required
+                type="text"
+                placeholder="Your Name"
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.name}
+              </Form.Control.Feedback>
             </Col>
-            <Form.Control.Feedback>Correct!</Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">
-              Please fill in this field.
-            </Form.Control.Feedback>
           </Form.Group>
         </Row>
         <Row className="mb-3 text-right">
-          <Form.Group as={Row} classname="mb-3" controlId="validationCustom02">
+          <Form.Group as={Row} controlId="validationCustom02">
             <Form.Label column sm="2">
               Email
             </Form.Label>
             <Col sm="10">
-              <Form.Control required type="email" placeholder="Your Email" />
+              <Form.Control
+                onChange={(e) => setField("email", e.target.value)}
+                isInvalid={!!errors.email}
+                required
+                type="email"
+                placeholder="Your Email"
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
             </Col>
-            <Form.Control.Feedback>Correct!</Form.Control.Feedback>
-            <Form.Control.Feedback type="invalid">
-              Please fill in this field.
-            </Form.Control.Feedback>
           </Form.Group>
         </Row>
         <div className="d-flex justify-content-end p-2">
